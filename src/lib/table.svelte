005ccdfd80530339a5fc560877954dbad443fcf4
jmattaa <mattajonathan1@gmail.com>
2025-03-31 17:49:12 +0200

    update table styling and project page

--- src/lib/table.svelte
+++ src/lib/table.svelte
@@ -2,29 +2,28 @@
     export let tableData;
 </script>
 
-<table class="table-auto w-full border-collapse border border-gray-300">
-    <thead>
-        <tr class="bg-gray-100">
-            {#each Object.keys(tableData[0]) as columnHeading}
-                <th class="px-4 py-2 border border-gray-300 text-left text-sm
-                font-medium text-gray-600"
-                >
-                    {columnHeading}
-                </th>
-            {/each}
-        </tr>
-    </thead>
-    <tbody>
-        {#each Object.values(tableData) as row}
-            <tr class="hover:bg-gray-50">
-                {#each Object.values(row) as cell}
-                    <td class="px-4 py-2 border border-gray-300 text-sm
-                    text-gray-700"
-                    >
-                        {cell}
-                    </td>
+<div class="overflow-x-auto p-4">
+    <table class="table-auto w-full border-collapse border border-gray-300 min-w-max shadow-lg rounded-lg">
+        <thead>
+            <tr>
+                {#each Object.keys(tableData[0]) as columnHeading}
+                    <th class="px-6 py-3 border border-gray-300 text-left text-sm font-semibold uppercase">
+                        {columnHeading}
+                    </th>
                 {/each}
             </tr>
-        {/each}
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            {#each Object.values(tableData) as row, index}
+                <tr class="{index % 2 === 0 ? 'bg-gray-100' : 'bg-white'} hover:bg-blue-100 transition duration-200">
+                    {#each Object.values(row) as cell}
+                        <td class="px-6 py-3 border border-gray-300 text-sm text-gray-700">
+                            {cell}
+                        </td>
+                    {/each}
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
+
