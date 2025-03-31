7e7c32a0a7efa439468238b2f66568f171348237
jmattaa <mattajonathan1@gmail.com>
2025-03-31 17:08:22 +0200

    add page transition and change hero

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -1,120 +1,114 @@
 <script>
-    // @ts-nocheck
-    import conf from "../tailwind-config";
-    import imagesos from "../assets/image-sos.png";
+	// @ts-nocheck
+	import conf from '../tailwind-config';
+	import imagesos from '../assets/image-sos.png';
+	import { page } from '$app/stores';
 
-    let menuState = false;
+	let menuState = false;
 
-    const toggleBurger = () => {
-        menuState = !menuState;
-    };
+	const toggleBurger = () => {
+		menuState = !menuState;
+	};
 
-    const navLinks = [
-        ["Hem", "/"],
-        ["Vilka är vi", "/about"],
-        ["Projekt", "/projekt"],
-        ["Stadgar", "/terms"],
-    ];
+	const navLinks = [
+		['Hem', '/'],
+		['Vilka är vi', '/about'],
+		['Projekt', '/projekt'],
+		['Stadgar', '/terms']
+	];
 
-    const onMouseEnter = (e) => {
-        e.target.style.fontVariationSettings =
-            '"MONO" .3, "CASL" var(--casl), "wght" 800, "slnt" .8, "CRSV" var(--CRSV)';
-    };
+	const onMouseEnter = (e) => {
+		e.target.style.fontVariationSettings =
+			'"MONO" .3, "CASL" var(--casl), "wght" 800, "slnt" .8, "CRSV" var(--CRSV)';
+	};
 
-    const onMouseLeave = (e) => {
-        e.target.style.fontVariationSettings =
-            '"MONO" var(--mono), "CASL" var(--casl), "wght" var(--wght), "slnt" var(--slnt), "CRSV" var(--CRSV)';
-    };
+	const onMouseLeave = (e) => {
+		e.target.style.fontVariationSettings =
+			'"MONO" var(--mono), "CASL" var(--casl), "wght" var(--wght), "slnt" var(--slnt), "CRSV" var(--CRSV)';
+	};
 </script>
 
 <nav
-    class="sticky top-0 left-8 z-50 w-full shadow-md bg-palette-5/10
+	class="sticky left-8 top-0 z-50 w-full bg-palette-5/10 shadow-md
                         backdrop-blur-3xl"
 >
-    <div class="flex justify-around items-center p-3">
-        <a href="/" class="text-lg font-extrabold text-palette-5">
-            <img src={imagesos} alt="Logo" width="125" height="100" />
-        </a>
+	<div class="flex items-center justify-around p-3">
+		<a href="/" class="text-lg font-extrabold text-palette-5">
+			<img src={imagesos} alt="Logo" width="125" height="100" />
+		</a>
 
-        <div class="hidden space-x-12 items-center lg:flex">
-            {#each navLinks as [label, href]}
-                <a
-                    {href}
-                    class="relative text-palette-3 hover:text-palette-5
-                    duration-500 transition-all after:content-['']
-                    after:flex after:absolute after:bottom-0 after:left-0
-                    after:w-full after:h-[.1em] after:bg-palette-5
-                    after:opacity-0 after:scale-x-0 after:origin-left
-                    after:transition-[opacity,transform] after:duration-300
-                    hover:after:opacity-100 hover:after:scale-x-[1]"
-                    onmouseenter={onMouseEnter}
-                    onmouseleave={onMouseLeave}
-                >
-                    {label}
-                </a>
-            {/each}
-        </div>
+		<div class="hidden items-center space-x-12 lg:flex">
+			{#each navLinks as [label, href]}
+				<a
+					{href}
+					class={`relative text-palette-3 transition-all
+                    duration-500 after:absolute after:bottom-0
+                    after:left-0 after:flex after:h-[.1em] after:w-full
+                    after:origin-left after:scale-x-0 after:bg-palette-5
+                    after:opacity-0 after:transition-[opacity,transform] after:duration-300
+                    after:content-[''] hover:text-palette-5
+                    hover:after:scale-x-[1] hover:after:opacity-100
+                    ${$page.url.pathname === href ? 'text-palette-5 after:scale-x-[1] after:opacity-100' : ''}`}
+					onmouseenter={onMouseEnter}
+					onmouseleave={onMouseLeave}
+				>
+					{label}
+				</a>
+			{/each}
+		</div>
 
-        <!-- NOTE: HELA mobil navbar linksen måste vara wrapped i button 
+		<!-- NOTE: HELA mobil navbar linksen måste vara wrapped i button 
                 elementet. Det är för att kunna stänga då man klickar vart som 
                 helst i öppna navigation link skiten.
         -->
-        <button
-            class="lg:hidden"
-            onclick={() => toggleBurger()}
-            aria-expanded={menuState}
-        >
-            <svg
-                width="32px"
-                height="32px"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <path
-                    d="M5 8H13.75M5 12H19M10.25 16L19 16"
-                    stroke={conf.theme.colors["palette-5"]}
-                    strokeLinecap="round"
-                />
-            </svg>
+		<button class="lg:hidden" onclick={() => toggleBurger()} aria-expanded={menuState}>
+			<svg
+				width="32px"
+				height="32px"
+				viewBox="0 0 24 24"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					d="M5 8H13.75M5 12H19M10.25 16L19 16"
+					stroke={conf.theme.colors['palette-5']}
+					strokeLinecap="round"
+				/>
+			</svg>
 
-            <!-- TODO: hitta ett sätt att kanske göra bg snyggare?? -->
-            <div
-                class={`
-                        flex flex-col items-center justify-center z-50
-                        fixed top-0 left-0 w-full h-0 bg-palette-2
+			<!-- TODO: hitta ett sätt att kanske göra bg snyggare?? -->
+			<div
+				class={`
+                        fixed left-0 top-0 z-50 flex
+                        h-0 w-full flex-col items-center justify-center bg-palette-2
                         transition-[height,opacity] duration-200
-                        ${
-                            menuState
-                                ? "flex !h-screen"
-                                : "opacity-0 h-0 pointer-events-none"
-                        }
+                        ${menuState ? 'flex !h-screen' : 'pointer-events-none h-0 opacity-0'}
                     `}
-            >
-                <div
-                    class="w-full h-full flex flex-col items-center
+			>
+				<div
+					class="flex h-full w-full flex-col items-center
                                         justify-center [&>*]:my-6"
-                >
-                    {#each navLinks as [label, href]}
-                        <a
-                            {href}
-                            class="relative text-black hover:text-palette-5
-                            duration-500 transition-all after:content-['']
-                            after:flex after:absolute after:bottom-0
-                            after:left-0 after:w-full after:h-[.1em]
-                            after:bg-palette-5 after:opacity-0 after:scale-x-0
-                            after:origin-left
-                            after:transition-[opacity,transform]
-                            after:duration-300 hover:after:opacity-100
-                            hover:after:scale-x-[1]"
-                            onmouseenter={onMouseEnter}
-                            onmouseleave={onMouseLeave}
-                        >
-                            {label}
-                        </a>
-                    {/each}
-                </div>
-            </div>
-        </button>
-    </div>
+				>
+					{#each navLinks as [label, href]}
+						<a
+							{href}
+							class="relative text-black transition-all
+                            duration-500 after:absolute after:bottom-0
+                            after:left-0 after:flex after:h-[.1em]
+                            after:w-full after:origin-left after:scale-x-0
+                            after:bg-palette-5 after:opacity-0 after:transition-[opacity,transform]
+                            after:duration-300
+                            after:content-['']
+                            hover:text-palette-5 hover:after:scale-x-[1]
+                            hover:after:opacity-100"
+							onmouseenter={onMouseEnter}
+							onmouseleave={onMouseLeave}
+						>
+							{label}
+						</a>
+					{/each}
+				</div>
+			</div>
+		</button>
+	</div>
 </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,22 @@
-<section class="relative h-screen flex flex-col items-center justify-center text-center text-white ">
-    <div class="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
-        <video class="min-w-full min-h-full absolute object-cover"
-            src="sosbarnbyar-hero.mp4"
-            autoplay muted loop></video>
-    </div>
-    <div class="video-content space-y-2 z-10">
-        <h1 class="font-extraboldbold text-6xl text-white">SOS Barnbyar TEG</h1>
-        <h3 class="font-light text-3xl text-white">För barnens framtid!</h3>
-    </div>
+<section class="relative flex h-screen flex-col items-center justify-center text-center text-white">
+	<div class="overlay absolute left-0 top-0 h-full w-full overflow-hidden">
+		<img src="/hero.jpg" alt="" class="absolute left-0 top-0 h-full w-full object-cover" />
+	</div>
+	<div class="z-10 space-y-2">
+		<h1 class="font-extraboldbold text-6xl text-white">SOS Barnbyar TEG</h1>
+		<h3 class="text-sm text-palette-2">(Skopje, Nord Makedonien på bilden)</h3>
+	</div>
 </section>
 
 <style>
-    .video-docker video {
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    }
-
-    .video-docker::after {
-        content: "";
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        left: 0;
-        background: rgba(0, 0, 0, 0.6);
-        z-index: 1;
-    }
+	.overlay::after {
+		content: '';
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		background: rgba(0, 0, 0, 0.8);
+		z-index: 1;
+	}
 </style>
